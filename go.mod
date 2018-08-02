5731069cffb0225dff32a85baa14d71971d78e79
Oliver Eilhard <oliver@eilhard.net>
2018-08-02 19:50:15 +0200

    Version 1.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/olivere/light
+
+require github.com/olivere/candle v0.1.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/olivere/candle"
+)
+
+func main() {
+	fmt.Printf("Candle version %s\n", candle.Version())
+}
